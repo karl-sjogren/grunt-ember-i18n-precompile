1f493087720011d3c5bc1267198f8537c30e06a7
Karl-Johan Sjogren <karl-johan.sjogren@openlibrarysolutions.com>
2014-02-18 13:38:17 +0100

    Updated tests

--- test/expected/en.js
+++ test/expected/en.js
@@ -1,27 +1,29 @@
+(function() {
+var t = Handlebars.template;
 Em.I18n.translations = {
     'group': {
         'create': {
-            'static': function (Handlebars,depth0,helpers,partials,data) {
+            'static': t(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   
 
 
   return "This one has escaped ' and is quoted with the same.";
-  },
-            "dynamic" : function (Handlebars,depth0,helpers,partials,data) {
+  }),
+            "dynamic" : t(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
+  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
 
 
   buffer += "This string has a \"";
-  if (stack1 = helpers.dynamic) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.dynamic); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
+  if (helper = helpers.dynamic) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.dynamic); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
     + "\" part.";
   return buffer;
-  }
+  })
         }
     }
-};
+};})();
